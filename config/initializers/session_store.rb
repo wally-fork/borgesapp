0468cf26eb65db1cdae6028c7512b87012011eb2
invertedplate <invertedplate@gmail.com>
2009-08-12 19:12:12 -0500

    fixed session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,10 +4,10 @@
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-# ActionController::Base.session = {
-#   :key         => '_borges_session',
-#   :secret      => '5d1c7b6fbea3337deaa7a878751db9de71d45b1c1d1b6d6dd0ce09836bf3d0083e054c8e92cefa993f4ab3ebf4cb67dcdac29d1b440a626d70ba8276bd688468'
-# }
+ActionController::Base.session = {
+  :key         => '_borges_session',
+  :secret      => '5d1c7b6fbea3337deaa7a878751db9de71d45b1c1d1b6d6dd0ce09836bf3d0083e054c8e92cefa993f4ab3ebf4cb67dcdac29d1b440a626d70ba8276bd688468'
+}
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
