bb2f92c8163e027967d177ceed91e4a27556b60b
invertedplate <invertedplate@gmail.com>
2009-08-14 13:29:47 -0500

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
   # UNCOMMENTED UNTIL I KNOW HOW TO POST WITH COOKIES FROM WITHIN EMACS
-  #   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
