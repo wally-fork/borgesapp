eba4d981bb604f22b5ff76af91a8077dd32656d2
mariko <mariko@mariko.(none)>
2009-09-08 01:47:41 -0500

    Modified environment.rb so that Passenger would recognize the PATH

--- config/environment.rb
+++ config/environment.rb
@@ -41,3 +41,6 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+# This is only for deployment with Passenger
+ENV['PATH'] = "#{ENV['PATH']}:/usr/local/texlive/2008/bin/i386-linux"
